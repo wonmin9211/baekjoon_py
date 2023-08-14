0779f694dbab027d45883660276c94e7b77cc016
wonmin9211 <wonmin9211@gmail.com>
2023-08-14 23:45:50 +0900

    first commit

--- 평범한 배낭.py
+++
@@ -1,7 +0,0 @@
-n,k=map(int, input().split()) # n=물건 갯수, k=버틸수 있는 무게
-a=[]
-for i in range(n):
-    w,v=map(int, input().split()) # w=물건 무게, v=물건의 가치
-    a.append(w)
-    for j in range(len(a)):
-        
